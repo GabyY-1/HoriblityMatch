f75ff5b5f38e748c643bb0d7d3c0b7ca9ca4c66b
GabyY-1 <gab.basque11@gmail.com>
2026-09-13 17:28:31 +0200

    Update Supabase URL to remove endpoint path

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://gkdsjtsqxbjgrlinpdba.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://gkdsjtsqxbjgrlinpdba.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_gEaFSPyCuUBCwo_8_RebPg_0P4F7QGW";
 
 const supabaseClient = window.supabase.createClient(
